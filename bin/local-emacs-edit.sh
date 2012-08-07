70d6d55fc32a652b28795f021806b66182d9d9a3
Ryan Davis <ryan@acceleration.net>
2012-08-07 10:53:06 -0400

    better emacs remote editing

--- bin/local-emacs-edit.sh
+++ bin/local-emacs-edit.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 if [ -n $ALTERNATE_EDITOR ]; then
     ALTERNATE_EDITOR=nano
 fi
@@ -7,25 +6,21 @@ fi
 if ! which emacsclient; then 
     echo "no emacsclient"
     $ALTERNATE_EDITOR $1
-    exit $?
+    exit
 fi
 
 if ! [ -r ~/.emacs.d/server/server ]; then
     echo "No server info file."
     $ALTERNATE_EDITOR $1
-    exit $?
+    exit
 fi
-    
-PORT=`egrep  -o '127.0.0.1:([0-9]*)' ~/.emacs.d/server/server | sed 's/127.0.0.1://'`
-if ! nc -z 127.0.0.1 $PORT; then  #anyone listening on that port?
-    echo "Port back to emacs server not open: $PORT"
-    $ALTERNATE_EDITOR $1
-    exit $?
-fi;    
 
 #build the tramp filename
-FN="/`whoami`@`hostname`:`ls -1 $(pwd)/$1`"
-echo "$FN"
+case $1 in
+     /*) FN="/`whoami`@`hostname`:$1" ;;
+     *) FN="/`whoami`@`hostname`:`pwd`/$1" ;;
+esac
+
 #we point it at the file written by the ssh wrapper for port and auth
 #information
-emacsclient -f ~/.emacs.d/server/server "$FN"
+emacsclient -a "$ALTERNATE_EDITOR" -f ~/.emacs.d/server/server "$FN"
