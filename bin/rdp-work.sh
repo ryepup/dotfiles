06b033438332c54ae52226a16df838b91bb587d8
Ryan Davis <ryan@mokeys.org>
2013-05-13 22:48:07 -0400

    modify rdesktop size

--- bin/rdp-work.sh
+++ bin/rdp-work.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rdesktop -u ryepup -d acceleration -a 16 -g 1280x960 -x b -r disk:home=/home/ryan/ ${1-10.201.35.213}
+rdesktop -u ryepup -d acceleration -a 16 -g 1600x1024 -x b -r disk:home=/home/ryan/ -r clipboard:CLIPBOARD ${1-10.201.35.213}
+
